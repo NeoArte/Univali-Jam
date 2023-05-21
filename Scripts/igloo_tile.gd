b87b46e1dfb09f562d2901bd842128dab6ee3d24
NeoArte <nicolasdestito74@gmail.com>
2023-05-21 01:39:41 -0300

    level 1 and 2

--- Scripts/igloo_tile.gd
+++ Scripts/igloo_tile.gd
@@ -1,14 +1,23 @@
 extends Area2D
 
-
+@export_file var next_level_path
+var s: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	s = load(next_level_path)
 	pass # Replace with function body.
 
 
 func _on_body_entered(body: PlayerClass):
-	body.add_level()
-
-
-func _on_Area2D_body_entered(body: PlayerClass):
-	get_tree().change_scene("<LevelBase>")
+	var root = get_tree().get_root()
+	var level = root.get_node('Level')
+	root.remove_child(level)
+	level.call_deferred('free')
+	
+	var next_level_res = load('res://Animations/Level1.res')
+	print('BATATATATATATATATATATATA')
+	var next_level = s.instantiate()
+	print(body.direction)
+	body.direction = Vector2.ZERO
+	body.state = 3
+	root.add_child(next_level)
